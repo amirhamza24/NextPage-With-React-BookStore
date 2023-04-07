8d91a41400a87f8be85252d269e324e125d6a6a5
amirhamza24 <ridoykhan7897890@gmail.com>
2023-04-07 16:24:03 +0600

    complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
-import Header from './Components/Header';
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
 
 const App = () => {
-  return (
-    <div>
-      {/* Header */}
-        <Header />
-        <Outlet />
-        {/* Footer */}
-    </div>
-  );
+    return (
+        <div>
+            {/* Header */}
+            <Header />
+
+            <div className="min-h-[calc(100vh - 136px)]">
+                <Outlet />
+            </div>
+
+            {/* Footer */}
+            <Footer />
+        </div>
+    );
 };
 
 export default App;
